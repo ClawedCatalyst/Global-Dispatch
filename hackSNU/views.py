e61b213e817e5423210630efa86e20f22e707822
suhaillahmad <suhaillahmadd0@gmail.com>
2023-01-27 17:51:05 +0530

    Login Done

--- hackSNU/views.py
+++ hackSNU/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.response import Response
+from . models import *
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+ 
+# API for sending OTP.   
+class OTP_send(generics.CreateAPIView):
+    serializer_class = OTP_Serializer
+    
+# OTP verification API.    
+class Verify_OTP(generics.CreateAPIView):
+    serializer_class = OTPVerifySerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Email Verified'}, status=status.HTTP_200_OK)
 
-# Create your views here.
+# Registration API
+class New_user_registration(generics.CreateAPIView):
+    serializer_class = NewUserSerializer
